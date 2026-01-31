1140e8a101625b9a5588d2104599c7693c5534c4
Arthur Lytvynov <3017810l@student.gla.ac.uk>
2026-01-31 23:47:22 +0000

    Chapter 4 done and tests passing

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'rango/index.html')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, "rango/about.html")
+    context_dict = {'boldmessage': 'This tutorial has been put together by ME.'}
+    return render(request, "rango/about.html", context=context_dict)
 
 
 
